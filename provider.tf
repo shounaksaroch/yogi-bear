37ea9dacb014c1c689aca9193edbccffd4f7cf59
Shounak Saroch <shounak.saroch@ingka.ikea.com>
2025-06-09 13:23:27 +0200

    SHSAR2 - FRESH - Need to specify env variables for terraform apply

--- provider.tf
+++ provider.tf
@@ -19,6 +19,9 @@ terraform {
 provider "azuread" {
 }
 
+provider "azurerm" {
+}
+
 # All configuration(where possible) is done via GIT repo variables/secrets
 ## ARM_CLIENT_ID      - secret
 ## ARM_CLIENT_SECRET  - secret
