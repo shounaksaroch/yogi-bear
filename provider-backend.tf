333fd27de6c227391fa9c26962ebb9387f26a769
Shounak Saroch <shounak.saroch@ingka.ikea.com>
2025-06-09 12:18:49 +0200

    SHSAR2 - Going back to AzureRM -- we want to use the same statefile in remotebackend to maintain state

--- provider-backend.tf
+++ provider-backend.tf
@@ -1,5 +1,20 @@
 terraform {
   #####
-  backend "local" {
+  ## We have configured the backend in our PROD subscription (GDNA@Test) so using AzureKeys to make life easier. This is not the recommended method
+  backend "azurerm" {
+    #access_key           = "abcdefghijklmnopqrstuvwxyz0123456789..."  # Can also be set via `ARM_ACCESS_KEY` environment variable.
+    # Will be passed in GITHUB workflow
+    #storage_account_name = "abcd1234"                                 # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
+    #container_name       = "tfstate"                                  # Can be passed via `-backend-config=`"container_name=<container name>"` in the `init` command.
+    #key                  = "prod.terraform.tfstate"                   # Can be passed via `-backend-config=`"key=<blob key name>"` in the `init` command.
   }
+  ###backend "azurerm" {
+  ###  #use_oidc             = true                                    # Can also be set via `ARM_USE_OIDC` environment variable.
+  ###  #use_azuread_auth     = true                                    # Can also be set via `ARM_USE_AZUREAD` environment variable.
+  ###  #tenant_id            = "00000000-0000-0000-0000-000000000000"  # Can also be set via `ARM_TENANT_ID` environment variable.
+  ###  #client_id            = "00000000-0000-0000-0000-000000000000"  # Can also be set via `ARM_CLIENT_ID` environment variable.
+  ###  storage_account_name = "abcd1234"                              # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
+  ###  container_name       = "tfstate"                               # Can be passed via `-backend-config=`"container_name=<container name>"` in the `init` command.
+  ###  key                  = "prod.terraform.tfstate"                # Can be passed via `-backend-config=`"key=<blob key name>"` in the `init` command.
+  ###}
 }
