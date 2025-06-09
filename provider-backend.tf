cd14839d2d7e444c6c5893088a43f1a2f08197b6
Shounak Saroch <shounak.saroch@ingka.ikea.com>
2025-06-09 21:05:09 +0200

    SHSAR2 - IWEOFTEST+OIDC - Now let's see

--- provider-backend.tf
+++ provider-backend.tf
@@ -8,9 +8,9 @@ terraform {
     #tenant_id            = "720b637a-655a-40cf-816a-f22f40755c2c"  # Can also be set via `ARM_TENANT_ID` environment variable.
     # Production client ID set manually here to avoid confusion with Test client ID
     #client_id            = "a7c35d02-5523-480f-8e2a-05116a52e0b9"  # Can also be set via `ARM_CLIENT_ID` environment variable.
-    #storage_account_name = "abcd1234"                              # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
-    #container_name       = "tfstate"                               # Can be passed via `-backend-config=`"container_name=<container name>"` in the `init` command.
-    #key                  = "prod.terraform.tfstate"                # Can be passed via `-backend-config=`"key=<blob key name>"` in the `init` command.
+    #storage_account_name = "zzzzz"                              # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
+    #container_name       = "yyyyy"                               # Can be passed via `-backend-config=`"container_name=<container name>"` in the `init` command.
+    #key                  = "xxxxx"                # Can be passed via `-backend-config=`"key=<blob key name>"` in the `init` command.
   }
 }
 
